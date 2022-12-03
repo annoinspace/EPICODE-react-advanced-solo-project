392a509e8a64035ebccf71e39492ac8e9ca2a3a3
annoinspace <aneesahalmask@gmail.com>
2022-12-03 17:23:44 +0000

    bottom component has some bugs

--- src/components/BottomDisplay.jsx
+++ src/components/BottomDisplay.jsx
@@ -1,10 +1,9 @@
-import React from "react"
 import { Row, Col } from "react-bootstrap"
 import { fromUnixTime } from "date-fns"
 
 export default function BottomDisplay({ main, sys }) {
-  const newsunrise = fromUnixTime(sys.sunrise)
-  const newsunset = fromUnixTime(sys.sunset)
+  //   const newsunrise = fromUnixTime(sys.sunrise)
+  //   const newsunset = fromUnixTime(sys.sunset)
 
   return (
     <Row className=" mb-5 mt-5 p-1 bottom-section">
@@ -13,14 +12,14 @@ export default function BottomDisplay({ main, sys }) {
           <div>
             <p>Sunrise</p>
           </div>
-          {sys ? <p>{newsunrise.toLocaleTimeString()}</p> : null}
+          {sys ? <p>{fromUnixTime(sys.sunrise)}</p> : null}
         </div>
         <div>
           <div>
             {" "}
             <p>Sunset</p>
           </div>
-          {sys ? <p>{newsunset.toLocaleTimeString()}</p> : null}
+          {sys ? <p>{fromUnixTime(sys.sunset)}</p> : null}
         </div>
       </Col>
       <Col
